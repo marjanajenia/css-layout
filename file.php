f397e71d10a2297d7dc8b14872649f949f067d21
unknown <marjanajenia98@gmail.com>
2021-04-05 19:33:26 +0600

    layout

---
+++ file.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="file.css">
+  
+</head>
+<body>
+	<main>
+		<div class="div1"> Header </div>
+		<div class="div2"> Nav </div>
+		<div class="div3"> Content </div>
+		<div class="div4"> Footer </div>
+	</main>
+
+</body>
+</html>
+
